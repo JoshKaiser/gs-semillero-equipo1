80b7cd0e349158cc6c2f3f61c4c63f78f16ef74b
Josue Garduño <JoshKaiser@users.noreply.github.com>
2019-12-19 11:13:46 -0600

    Clase TiendaService
    Se añadio la clase de servicio para la tienda en linea

---
+++ servicios/src/main/java/com/gs/bdm_tienda_en_linea/business/RegistroClienteBusiness.java
@@ -0,0 +1,10 @@
+package com.gs.bdm_tienda_en_linea.business;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistroClienteBusiness {
+	
+	//@Autowired
+	
+}
--- servicios/src/main/java/com/gs/bdm_tienda_en_linea/service/TiendaService.java
+++ servicios/src/main/java/com/gs/bdm_tienda_en_linea/service/TiendaService.java
@@ -1,5 +1,31 @@
 package com.gs.bdm_tienda_en_linea.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gs.bdm_tienda_en_linea.business.RegistroClienteBusiness;
+
+import lombok.extern.slf4j.Slf4j;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value="/api/bdm/tiendapp")
+@Slf4j
 public class TiendaService {
+
+	@Autowired
+	RegistroClienteBusiness registroCliente;
+	
+	@RequestMapping(value="registrodeusuario", method=RequestMethod.POST, consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> registrarCliente(@RequestBody String request, HttpServletRequest httpServlet){
+		return (ResponseEntity<String>) registroCliente
+	}
+	
 
 }
