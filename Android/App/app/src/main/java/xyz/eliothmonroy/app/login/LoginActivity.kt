336f5c59c6129219c7bd7e0e898978b7b430b786
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-12-18 18:20:10 -0600

    Add login screen

---
+++ Android/App/app/src/main/java/xyz/eliothmonroy/app/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package xyz.eliothmonroy.app.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.eliothmonroy.app.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
